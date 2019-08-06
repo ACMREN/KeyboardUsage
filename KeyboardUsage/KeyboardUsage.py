7baf37a6602178282e1e8fcdab8a238d4119723c
acmren <414443644@qq.com>
2019-08-06 18:25:34 +0800

    修改了arr

--- KeyboardUsage/KeyboardUsage.py
+++ KeyboardUsage/KeyboardUsage.py
@@ -3,10 +3,20 @@ import sys
 import MySQLdb
 
 dict = {};
-
+char_arr = [];
 
 def on_press(key):
     try:
+        global char_arr
+        print(dict)
+        print(char_arr)
+        if(len(char_arr) < 3):
+            char_arr.append(key.char)
+        else:
+            if(char_arr[0] == 'ctrl_l' and char_arr[1] == 'c' and char_arr[2] == 'd'):
+                return False
+            else:
+                char_arr = []
         if(dict.get(str(key.char)) == None):
             dict[str(key.char)] = 1
         else:
@@ -14,6 +24,10 @@ def on_press(key):
         print(dict)
     except AttributeError:
         keyStr = str(key).split('.')[1]
+        if(len(char_arr) < 3):
+            char_arr.append(keyStr)
+        else:
+            char_arr = []
         if(dict.get(keyStr) == None):
             dict[keyStr] = 1
         else:
@@ -22,31 +36,17 @@ def on_press(key):
             return False
         print(dict)
 
-#def on_release(key):
-#    print('{0} released'.format(key))
+listener = keyboard.Listener(on_press=on_press)
 
 def openMysqlConnection():
-    conn = MySQLdb.connect('localhost', 'root', 'root', 'sakila', charset='utf8');
+    conn = MySQLdb.connect('localhost', 'root', 'root', 'testdb', charset='utf8');
     print(conn)
     cursor = conn.cursor()
-    cursor.execute("select * from actor")
-    result = cursor.fetchall();
-    for item in result:
-        print(item)
     cursor.close()
     return conn;
 
 def closeMysqlConnection(db):
     db.close()
-
-        
-# #Collect events until released
-#with keyboard.Listener(
-#        on_press=on_press) as listener:
-#    listener.join()
-
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(on_press=on_press)
 
 def main():
     listener.start()
