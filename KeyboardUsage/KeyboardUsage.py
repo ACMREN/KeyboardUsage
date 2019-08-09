d4bce92d284a0cb3695f4f0bd3171d56aeeb391c
acmren <414443644@qq.com>
2019-08-09 19:08:48 +0800

    新增判断是否存在已有的记录

--- KeyboardUsage/KeyboardUsage.py
+++ KeyboardUsage/KeyboardUsage.py
@@ -1,43 +1,14 @@
 from pynput import keyboard
 import sys
 import MySQLdb
+import datetime
+import json
+from PIL import Image
 
 dict = {};
 char_arr = [];
 
-def on_press(key):
-    try:
-        global char_arr
-        print(dict)
-        print(char_arr)
-        if(len(char_arr) < 3):
-            char_arr.append(key.char)
-        else:
-            if(char_arr[0] == 'ctrl_l' and char_arr[1] == 'c' and char_arr[2] == 'd'):
-                return False
-            else:
-                char_arr = []
-        if(dict.get(str(key.char)) == None):
-            dict[str(key.char)] = 1
-        else:
-            dict[str(key.char)] += 1
-        print(dict)
-    except AttributeError:
-        keyStr = str(key).split('.')[1]
-        if(len(char_arr) < 3):
-            char_arr.append(keyStr)
-        else:
-            char_arr = []
-        if(dict.get(keyStr) == None):
-            dict[keyStr] = 1
-        else:
-            dict[keyStr] += 1
-        if(key == key.esc):
-            return False
-        print(dict)
-
-listener = keyboard.Listener(on_press=on_press)
-
+# 打开数据库，并返回一个数据库连接对象
 def openMysqlConnection():
     conn = MySQLdb.connect('localhost', 'root', 'root', 'testdb', charset='utf8');
     print(conn)
@@ -45,21 +16,79 @@ def openMysqlConnection():
     cursor.close()
     return conn;
 
+# 关闭数据库连接
 def closeMysqlConnection(db):
     db.close()
 
-def main():
-    listener.start()
-    print("main run")
+# 保存键盘使用的数据
+def saveUsageRecord():
     conn = openMysqlConnection()
     cursor = conn.cursor()
-    cursor.execute("select version()")
-    result = cursor.fetchall();
-    for item in result:
-        print(item)
+    now_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    dictJson = json.dumps(dict)
+    data = str(dictJson)
+    sql = "INSERT keyboard_usage(recordData, createTime) VALUES (%s, %s)"
+    print(sql)
+    cursor.execute(sql,(data, now_date))
+    cursor.close()
+    conn.commit()
+    closeMysqlConnection(conn)
+
+# 退出键盘监听，并调用保存数据的方法
+def exitListener(char_arr, keyStr):
+    if(keyStr == 'pause'):
+        saveUsageRecord()
+        return False
+    #if(keyStr == 'ctrl_l' and len(char_arr) == 0):
+    #    char_arr.append(keyStr)
+    #elif(len(char_arr) > 0 and len(char_arr) < 3 and char_arr[0] == 'ctrl_l'):
+    #    char_arr.append(keyStr)
+    #if(len(char_arr) == 3 and char_arr[0] == 'ctrl_l' and char_arr[1] == 'c' and char_arr[2] == 'd'):
+    #    saveUsageRecord()
+    #    return False
+    #else:
+    #    char_arr.clear()
+
+# 键盘监听的主要方法
+def on_press(key):
+    try:
+        if(dict.get(str(key.char)) == None):
+            dict[str(key.char)] = 1
+        else:
+            dict[str(key.char)] += 1
+        print(dict)
+        isExit = exitListener(char_arr, key.char)
+        if(isExit == False):
+            return False
+    except AttributeError:
+        keyStr = str(key).split('.')[1]
+        if(dict.get(keyStr) == None):
+            dict[keyStr] = 1
+        else:
+            dict[keyStr] += 1
+        print(dict)
+        isExit = exitListener(char_arr, keyStr)
+        if(isExit == False):
+            return False
+
+# 监听器的注册
+listener = keyboard.Listener(on_press=on_press)
+
+def main():
+    listener.start()
+    # 判断今天是否已经存有数据，如果有的话，则dict为今天数据
+    # 如果没有，则dict保持为新对象
+    conn = openMysqlConnection()
+    cur = conn.cursor()
+    now_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    sql = "select * from keyboard_usage where createTime = %s"
+    cur.execute(sql, now_date)
+    result = cur.fetchone()
+    if(result != None):
+        dict = result[2]
+    conn.close()
     listener.join()
         
-
 if __name__ == "__main__":
     main()
 
